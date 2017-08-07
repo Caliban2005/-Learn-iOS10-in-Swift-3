741c755c5173aa318306159ac92ee5ceefc5021c
Ulf Springer <ulf_springer@Ulfs-MacBook-Pro.local>
2017-08-07 12:05:53 +0100

    Initial Commit

---
+++ Github_Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github_Testing
+//
+//  Created by Ulf Springer on 07/08/2017.
+//  Copyright © 2017 Ulf Springer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
